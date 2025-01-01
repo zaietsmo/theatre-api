f33a272857dcecc5a38b764f83d21b05d51bf11c
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2025-01-02 00:30:12 +0200

    feat: added views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
