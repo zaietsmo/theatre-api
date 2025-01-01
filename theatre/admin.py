c14eb96c9b70b46f938a157968e2139af185125f
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2025-01-02 00:32:27 +0200

    feat: added models to admin-panel

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Actor, Genre, Performance, Play, Reservation, TheatreHall, Ticket
+
+admin.site.register(Play)
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Performance)
+admin.site.register(TheatreHall)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
